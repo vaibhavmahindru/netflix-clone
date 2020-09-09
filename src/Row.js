5994c5850caed04197519ea8407076df24d582ba
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-09-10 03:23:26 +0530

    rows done

---
+++ src/Row.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, largeRow }) {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  return (
+    <React.Fragment>
+      <div className="row">
+        <h2>{title}</h2>
+        <div className="row-posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              src={`${base_url}${
+                largeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+              className={`row-poster ${largeRow && "row-posterLarge"}`}
+            />
+          ))}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Row;
